9b5512fa90027c5df3b2959a466164e7e24e6d21
Martynenko Oleksandr <48642804+lHappYzz@users.noreply.github.com>
2022-05-14 07:13:40 +0300

    clear default state handler

--- app/States/DefaultState.php
+++ app/States/DefaultState.php
@@ -3,18 +3,12 @@
 namespace App\States;
 
 use App\Bot;
-use App\Records\StatusRecord;
 use Boot\Src\Abstracts\State;
 
 class DefaultState extends State
 {
     public function handle(Bot $bot): void
     {
-        $bot->sendMessage('Current state: Default', $bot->getChat());
-
-        $statusRecord = StatusRecord::fetch(StatusRecord::POST_SUGGESTION);
-        $this->telegramChat->setStatus($statusRecord);
-
-        $bot->sendMessage('State changed to: post_suggestion', $bot->getChat());
+        //handle chats with default status
     }
 }
