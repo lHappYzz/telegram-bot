14edafe4fcd9e7edf95660702d8bae62e7d977b2
Oleksandr <ssashsa96@gmail.com>
2023-09-27 20:01:10 +0300

    Moved webhook functions to the Bot class. Changed TelegramRequest Update retrieving logic

--- boot/Src/TelegramRequest.php
+++ boot/Src/TelegramRequest.php
@@ -2,7 +2,6 @@
 
 namespace Boot\Src;
 
-use App\Config\Config;
 use Boot\TelegramUpdateParser;
 use Boot\Traits\Http;
 
@@ -13,37 +12,21 @@ class TelegramRequest
     /**
      * An object created from JSON sent by telegram
      *
-     * @var Update
+     * @var Update|null
      * @see parseTelegramRequest
      */
-    protected Update $update;
-
-    public function __construct(TelegramUpdateParser $telegramUpdateParser)
-    {
-        $this->update = $telegramUpdateParser->parseTelegramRequest()->createUpdate();
-    }
+    private ?Update $update = null;
 
     /**
+     * Parse Update from php://input
+     *
      * @return Update
      */
     public function getUpdate(): Update
     {
-        return $this->update;
-    }
-
-    /**
-     * Set telegram webhook according to application configuration
-     */
-    public static function setWebhook()
-    {
-        $botConfig = Config::bot();
-        return self::sendTelegramRequest(['token' => $botConfig['bot_token'], 'method' => 'setWebhook', 'url' => 'https://' . $botConfig['bot_url']]);
-    }
-    /**
-     * Remove telegram webhook
-     */
-    public static function removeWebhook()
-    {
-        return self::sendTelegramRequest(['token' => Config::bot()['bot_token'], 'method' => 'setWebhook', 'url' => '']);
+        return $this->update ?? $this->update =
+                container(TelegramUpdateParser::class)
+                    ->parseTelegramRequest()
+                    ->createUpdate();
     }
 }
